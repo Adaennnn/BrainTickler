c9054b822ad5410394038b2a48aad9686b792147
yardenvdg-gloat <yarden.vdg@gloat.com>
2023-10-28 21:03:01 +0300

    refactored custom hook useCorrectAnswers

--- src/hooks/useCorrectAnswers.jsx
+++ src/hooks/useCorrectAnswers.jsx
@@ -2,31 +2,25 @@ import React from "react";
 
 // Custom hook to determine correct answers provided by the user in a quiz.
 export const useCorrectAnswers = (quizUserAnswers, quizCorrectAnswers, answersSubmited) => {
-    // State to hold user-provided correct answers
     const [correctUserAnswers, setCorrectUserAnswers] = React.useState([]);
 
-    // Update state based on quizUserAnswers and quizCorrectAnswers
     React.useEffect(() => {
-        // Temporary array to hold correct answers
-        let tempCorrectUserAnswers = [];
-
-        // Convert user answers object to an array for easier comparison
-        const quizUserAnswersArray = Object.values(quizUserAnswers);
-
-        // Loop through each correct answer to see if the user got it right
-        for (let i = 0; i < quizCorrectAnswers.length; i++) {
-            const currentCorrectAnswer = quizCorrectAnswers[i];
-            const currentUserAnswer = quizUserAnswersArray[i];
-
-            if (currentCorrectAnswer === currentUserAnswer) {
-                tempCorrectUserAnswers.push(currentUserAnswer);
-            }
+        // If the lengths aren't the same, they can't be equal
+        if (quizCorrectAnswers.length !== Object.keys(quizUserAnswers).length) {
+            setCorrectUserAnswers([]);
+            return;
         }
 
-        // Update the state with correct answers
+        // Convert user answers object to an array
+        const quizUserAnswersArray = Object.values(quizUserAnswers);
+
+        // Find and collect correct answers
+        const tempCorrectUserAnswers = quizCorrectAnswers.filter((correctAnswer, index) => {
+            return correctAnswer === quizUserAnswersArray[index];
+        });
+
         setCorrectUserAnswers(tempCorrectUserAnswers);
     }, [answersSubmited]);
 
-    // Return array of correct answers
     return correctUserAnswers;
 };
